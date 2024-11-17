8fd7b25133f8a842be85ca3dc96c4d1684b2e0ba
Ign4R <69459950+Ign4R@users.noreply.github.com>
2024-11-17 17:03:24 -0300

    add sprites

--- Assets/Celular.cs
+++ Assets/Celular.cs
@@ -2,10 +2,24 @@ using UnityEngine;
 
 public class Celular : ObjetoSimple
 {
+    private bool isCharged;
+
     private void OnMouseDown()
     {
         agarrar();
         gameObject.SetActive(false);
         itemUI.gameObject.SetActive(true);
     } 
+
+    public void ChangeState()
+    {
+        if (isCharged)
+        {
+            //TODO: CAMBIAR SPRITE
+        }
+        else
+        {
+            //TODO: CAMBIAR SPRITE
+        }
+    }
 }
