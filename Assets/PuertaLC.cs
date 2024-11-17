b0d7e6cff38bc452728690ca25e3d7668e3c7eb0
Florencia Cardinali <florenciapaulacardinali@gmail.com>
2024-11-17 16:50:21 -0300

    puerta bloqueda con la llave
    asdasd

--- Assets/PuertaLC.cs
+++ Assets/PuertaLC.cs
@@ -4,10 +4,17 @@ public class PuertaLC : ObjetoSimple
 {
     [SerializeField] GameObject camMain;
     [SerializeField] GameObject camDireccion;
+    [SerializeField] bool abierta;
+    [SerializeField] Inventario inven;
+
+
     private void OnMouseDown()
     {
-        camMain.SetActive(false);
-        camDireccion.SetActive(true);
+        if (inven.consultaPorNombre("Llave") || abierta)
+        {
+            camMain.SetActive(false);
+            camDireccion.SetActive(true);
+        }      
     }
 }
 
